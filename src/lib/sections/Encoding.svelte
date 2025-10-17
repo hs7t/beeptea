8500465eb0b6d07ac7836e8542ee0f6b44ba2cb7
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-17 18:42:42 -0500

    create Encoder component

---
+++ src/lib/sections/Encoding.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { encodeString, decodeString } from '../../logic/morse'
+    let encoderTextInput = $state("")
+    let encodedInput = $derived(encodeString(encoderTextInput))
+</script>
+
+<section class="area encoding-area">
+    <textarea bind:value={encoderTextInput}></textarea>
+    <div>
+        <p>{encodedInput}</p>
+    </div>
+</section>
