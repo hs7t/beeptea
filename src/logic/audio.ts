6e9196d99e7f76ef7e68c81f4e54b2681d46ddc6
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-17 20:22:10 -0500

    expose playTestSound in audio.ts

--- src/logic/audio.ts
+++ src/logic/audio.ts
@@ -2,6 +2,6 @@ import * as Tone from 'tone'
 
 const synth = new Tone.Synth().toDestination();
 
-const playTestSound = () => {
+export const playTestSound = () => {
     synth.triggerAttackRelease("C4", "8n");
 }
