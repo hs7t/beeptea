223235b0b97a4e7012986b783e47c5431bcee5ea
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-17 20:20:28 -0500

    add Trainwreck component for testing

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import Encoding from "./lib/sections/Encoding.svelte";
-
+  import Trainwreck from "./lib/sections/Trainwreck.svelte";
 </script>
 
 <main>
     <Encoding />
+    <Trainwreck />
 </main>
 
 <style>
