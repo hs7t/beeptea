9e3ac7bf2db7c7edc93a21bcad1bfef6075a5eb7
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-18 14:01:21 -0500

    make downloadTestSound() work

--- src/logic/audio.ts
+++ src/logic/audio.ts
@@ -3,10 +3,18 @@ import * as Tone from 'tone'
 const synth = new Tone.Synth().toDestination();
 const recorder = new Tone.Recorder()
 
-export const playTestSound = () => {
-    synth.triggerAttackRelease(500, "8n");
+export const playTestSound = async () => {
+    await synth.triggerAttackRelease(500, "8n");
 }
 
-export const downloadTestSound = () => {
+export const downloadTestSound = async () => {
     recorder.start()
+    await playTestSound()
+    const recording = await recorder.stop()
+    const recordingURL = URL.createObjectURL(recording)
+    const anchor = document.createElement("a")
+	anchor.download = "recording.webm";
+	anchor.href = recordingURL;
+	anchor.click();
+
 }
