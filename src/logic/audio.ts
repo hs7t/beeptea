d24f87378644a30151432af7c872029331a13f69
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-18 14:22:24 -0500

    fix recorder functionality

--- src/logic/audio.ts
+++ src/logic/audio.ts
@@ -1,15 +1,19 @@
 import * as Tone from 'tone'
+import * as Tools from './tools'
 
 const synth = new Tone.Synth().toDestination();
 const recorder = new Tone.Recorder()
 
-export const playTestSound = async () => {
-    await synth.triggerAttackRelease(500, "8n");
+synth.connect(recorder)
+
+export const playSynthFrequency = async (frequency: number, time: string) => {
+    await synth.triggerAttackRelease(frequency, time);
+    await Tools.wait(Tone.Time(time).toMilliseconds())
 }
 
 export const downloadTestSound = async () => {
     recorder.start()
-    await playTestSound()
+    await playSynthFrequency(500, "8n")
     const recording = await recorder.stop()
     const recordingURL = URL.createObjectURL(recording)
     const anchor = document.createElement("a")
