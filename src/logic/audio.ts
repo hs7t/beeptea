811824321ecdb5c7f8437f485fbe15aaa803e4af
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-17 20:28:15 -0500

    add recorder to audio.ts

--- src/logic/audio.ts
+++ src/logic/audio.ts
@@ -1,7 +1,12 @@
 import * as Tone from 'tone'
 
 const synth = new Tone.Synth().toDestination();
+const recorder = new Tone.Recorder()
 
 export const playTestSound = () => {
     synth.triggerAttackRelease("C4", "8n");
+}
+
+export const downloadTestSound = () => {
+    recorder.start()
 }
