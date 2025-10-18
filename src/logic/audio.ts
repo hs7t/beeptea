7c8ab2a9ae7cde3a93cbf4d37192a481478948b0
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-18 14:25:25 -0500

    remove semicolons in audio.ts

--- src/logic/audio.ts
+++ src/logic/audio.ts
@@ -1,13 +1,13 @@
 import * as Tone from 'tone'
 import * as Tools from './tools'
 
-const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Synth().toDestination()
 const recorder = new Tone.Recorder()
 
 synth.connect(recorder)
 
 export const playSynthFrequency = async (frequency: number, time: string) => {
-    await synth.triggerAttackRelease(frequency, time);
+    await synth.triggerAttackRelease(frequency, time)
     await Tools.wait(Tone.Time(time).toMilliseconds())
 }
 
@@ -17,8 +17,7 @@ export const downloadTestSound = async () => {
     const recording = await recorder.stop()
     const recordingURL = URL.createObjectURL(recording)
     const anchor = document.createElement("a")
-	anchor.download = "recording.webm";
-	anchor.href = recordingURL;
-	anchor.click();
-
+	anchor.download = "recording.webm"
+	anchor.href = recordingURL
+	anchor.click()
 }
