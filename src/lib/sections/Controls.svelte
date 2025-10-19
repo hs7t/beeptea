55c7f0c5be4ff4b9cdb18be020567855317c4e9f
Alí Sinisterra <haidarconh@gmail.com>
2025-10-19 18:59:50 -0500

    display input for every tuneOptions.ranges

--- src/lib/sections/Controls.svelte
+++ src/lib/sections/Controls.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
     import { appState } from "../../shared.svelte";
+    import FromToNumberInput from "../components/inputs/FromToNumberInput.svelte"
+    import Labeled from "../components/Labeled.svelte"
 </script>
 
 <section class="area controls-area">
     {#each Object.entries(appState.tuneOptions.ranges) as [key, range]}
-        {key}
+        <Labeled labelText={key} labeledID={`${key}-range-input`}>
+            <FromToNumberInput id={`${key}-range-input`}></FromToNumberInput>
+        </Labeled>
     {/each}
 </section>
