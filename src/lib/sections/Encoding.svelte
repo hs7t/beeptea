29fe5ecf52c663c80b5a1fd94a75074fa756cf72
Alí Sinisterra <haidarconh@gmail.com>
2025-10-19 14:41:37 -0500

    use new appState in Encoding.svelte

--- src/lib/sections/Encoding.svelte
+++ src/lib/sections/Encoding.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     import { encodeString } from '../../logic/morse'
-    let encoderTextInput = $state("")
-    let encodedInput = $derived(encodeString(encoderTextInput))
+    import { appState } from '../../shared.svelte'
+    let encodedString = $derived(encodeString(appState.currentInputString))
 </script>
 
 <section class="area encoding-area">
-    <textarea bind:value={encoderTextInput}></textarea>
+    <textarea bind:value={appState.currentInputString}></textarea>
     <div>
-        <p>{encodedInput}</p>
+        <p>{encodedString}</p>
     </div>
 </section>
