c49c2a7255cba0e3f65a374a4669e114797c911c
Alí Sinisterra <haidarconh@gmail.com>
2025-10-19 16:31:11 -0500

    add basic functionality to FromToNumberInput

---
+++ src/lib/components/inputs/FromToNumberInput.svelte
@@ -0,0 +1,8 @@
+<script>
+    let { from = $bindable(), to = $bindable() } = $props()
+</script>
+
+<div>
+    from <input type="number" bind:value={from}>
+    to <input type="number" bind:value={to}>
+</div>
