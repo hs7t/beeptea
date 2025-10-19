612dd6a3ab978b0fd7a514ecfaab7d983ab019db
Alí Sinisterra <haidarconh@gmail.com>
2025-10-19 15:57:52 -0500

    add tuneOptions to appState in shared.svelte.ts

--- src/shared.svelte.ts
+++ src/shared.svelte.ts
@@ -1,3 +1,9 @@
+import type { MorseFrequencyRanges } from './logic/audio'
+import { defaultMorseRanges } from './logic/audio'
+
 export let appState = $state({
     currentInputString: 'Hello, world!',
+    tuneOptions: {
+        ranges: defaultMorseRanges as MorseFrequencyRanges,
+    },
 })
