7f851580b2d0fa1fc39eac5bb7fc9838b23b2536
Alí Sinisterra <haidarconh@gmail.com>
2025-10-19 13:32:04 -0500

    add getRandom function to Range class in tools.ts

--- src/logic/tools.ts
+++ src/logic/tools.ts
@@ -1,25 +1,29 @@
 export class Range {
-  start: number;
-  end: number;
+    start: number
+    end: number
 
-  constructor(from: number, to: number) {
-    this.start = from;
-    this.end = to;
-  }
-
-  size(): number {
-    return this.end - this.start;
-  }
-
-  all(): Array<number> {
-    let rangeArray = [];
-    for (let i = 0; i == this.size(); i++) {
-      rangeArray.push(i + this.start);
+    constructor(from: number, to: number) {
+        this.start = from
+        this.end = to
     }
-    return rangeArray;
-  }
+
+    size(): number {
+        return this.end - this.start
+    }
+
+    all(): Array<number> {
+        let rangeArray = []
+        for (let i = 0; i == this.size(); i++) {
+            rangeArray.push(i + this.start)
+        }
+        return rangeArray
+    }
+
+    getRandom(): number {
+        return Math.random() * (this.end - this.start) + this.start
+    }
 }
 
 export const wait = (milliseconds: number) => {
-  return new Promise((resolve) => setTimeout(resolve, milliseconds));
-};
+    return new Promise((resolve) => setTimeout(resolve, milliseconds))
+}
