8dd45e4103de83db25645b6b30e3b32a9415a6da
Alí Sinisterra <haidarconh@gmail.com>
2025-10-19 14:43:36 -0500

    use new tune code in button

--- src/lib/sections/Trainwreck.svelte
+++ src/lib/sections/Trainwreck.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-    import {downloadTestSound, playSynthFrequency} from '../../logic/audio'
+    import { downloadMorseTune, playSynthFrequency } from '../../logic/audio'
+    import { encodeString } from '../../logic/morse';
+    import { appState } from '../../shared.svelte'
 </script>
 
 <section>
     <button onclick={() => playSynthFrequency(500, "8n")}>play sound</button>
-    <button onclick={downloadTestSound}>download sound</button>
+    <button onclick={() => downloadMorseTune(encodeString(appState.currentInputString))}>download sound</button>
 </section>
