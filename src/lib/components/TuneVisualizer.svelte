c0ef4448c8e2d73b57d63deb851d13d2cb6ae349
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-20 18:39:52 -0500

    create TuneVisualizer, a copy of TextVisualizer

---
+++ src/lib/components/TuneVisualizer.svelte
@@ -0,0 +1,38 @@
+<script>
+    let { text } = $props()
+</script>
+
+<div class="tune-visualizer">
+    <p>{text}</p>
+</div>
+
+<style>
+    .tune-visualizer {
+        overflow-x: scroll;
+        display: flex;
+        align-items: center;
+        border-radius: var(--b-radius-small);
+
+        padding: 0.2rem;
+        background-color: var(--b-color-overlay-background);
+    }
+
+    .tune-visualizer p {
+        text-align: center;
+        white-space: nowrap;
+    }
+
+    .tune-visualizer::-webkit-scrollbar {
+        width: 0.6rem;
+    }
+
+    .tune-visualizer::-webkit-scrollbar-track {
+        background: #e3e3e3;
+        border-radius: 10pt;
+    }
+    
+    .tune-visualizer::-webkit-scrollbar-thumb {
+        background: #b3b3b3;
+        border-radius: 10pt;
+    }
+</style>
