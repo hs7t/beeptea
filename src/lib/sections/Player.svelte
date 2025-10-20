ea198c38eaeae3c7ca68db18dc3812cf7b11cafd
Alí Sinisterra <haidarconh@gmail.com>
2025-10-20 17:07:09 -0500

    add recorder toggling functionality to Player

--- src/lib/sections/Player.svelte
+++ src/lib/sections/Player.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
     import { playAndRecord } from '../../logic/audio'
-    import { appState } from '../../shared.svelte'
+    import { appState, toggleRecorder } from '../../shared.svelte'
+
+    let recordingEnabled = $state(false)
+
+    $effect(() => {
+        if (appState.recorder != undefined) {
+            recordingEnabled = true
+        } else {
+            recordingEnabled = false
+        }
+    })
 </script>
 
 <section>
+    <button onclick={() => toggleRecorder()}>Toggle recording (enabled: {recordingEnabled})</button>
     <button onclick={() => playAndRecord(appState.tune, appState.recorder)}>Play</button>
 </section>
