25dd559885773e5aedae58358f2f4e9c4da7da84
Alí Sinisterra <haidarconh@gmail.com>
2025-10-20 14:10:32 -0500

    write Editing component

---
+++ src/lib/sections/Editing.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { tuneToNotation } from "../../logic/audio"
+    import { appState } from "../../shared.svelte"
+
+    let notation = $derived(tuneToNotation(appState.tune))
+</script>
+<div>
+    <p>{notation}</p>
+    <textarea></textarea>
+</div>
