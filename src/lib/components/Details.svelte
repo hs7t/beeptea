8a2e42f5785d2be67871f35d05bdff7b9c8cf305
Alí Sinisterra <haidarconh@gmail.com>
2025-10-20 16:11:28 -0500

    style Details.svelte

--- src/lib/components/Details.svelte
+++ src/lib/components/Details.svelte
@@ -6,3 +6,26 @@
     <summary>{summary}</summary>
     {@render children?.()}
 </details>
+
+<style>
+    details {
+        border: var(--b-border-secondary);
+        border-radius: 4px;
+        padding: 0.5em 0.5em 0;
+    }
+
+    summary {
+        margin: -0.5em -0.5em 0;
+        padding: 0.5em;
+    }
+
+    details[open] {
+        padding: 0.5em;
+    }
+
+    details[open] summary {
+        border-bottom: var(--b-border-secondary);
+        margin-bottom: 0.5em;
+    }
+
+</style>
