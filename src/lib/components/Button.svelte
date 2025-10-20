7f5c76791bcf267806fa0866758e7e51f319534c
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-20 18:31:58 -0500

    style Button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -22,5 +22,18 @@
         background-color: var(--b-color-overlay-background);
         border: var(--b-border-secondary);
         border-radius: var(--b-radius-small);
+
+        transition: all;
+
+        display: inline-flex;
+    }
+
+    button:hover {
+        background-color: #e1e1e1;
+        transition: all 100ms;
+    }
+
+    button:active {
+        transform: translateY(1pt);
     }
 </style>
