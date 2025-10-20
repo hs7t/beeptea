fd69948198b0fca60cfc6520178fb7a3424f4e8d
Alí Sinisterra <haidarconh@gmail.com>
2025-10-19 19:28:03 -0500

    use new Details component in Controls

--- src/lib/sections/Controls.svelte
+++ src/lib/sections/Controls.svelte
@@ -2,20 +2,24 @@
     import { appState } from "../../shared.svelte";
     import FromToNumberInput from "../components/inputs/FromToNumberInput.svelte"
     import Fieldset from "../components/Fieldset.svelte"
+    import Details from "../components/Details.svelte"
 </script>
 
 <section class="area controls-area">
-    {#each Object.entries(appState.tuneOptions.ranges) as [key, range]}
-        <Fieldset labelText={key}>
-            <FromToNumberInput 
-                id={`${key}-range-input`} 
-                bind:from={
-                    appState.tuneOptions.ranges[key as keyof typeof appState.tuneOptions.ranges].start
-                } 
-                bind:to={
-                    appState.tuneOptions.ranges[key as keyof typeof appState.tuneOptions.ranges].end
-                }
-            ></FromToNumberInput>
-        </Fieldset>
-    {/each}
+    <h2>Options</h2>
+    <Details summary="Frequency ranges">
+        {#each Object.entries(appState.tuneOptions.ranges) as [key, range]}
+            <Fieldset labelText={`${key}`}>
+                <FromToNumberInput 
+                    id={`${key}-range-input`} 
+                    bind:from={
+                        appState.tuneOptions.ranges[key as keyof typeof appState.tuneOptions.ranges].start
+                    } 
+                    bind:to={
+                        appState.tuneOptions.ranges[key as keyof typeof appState.tuneOptions.ranges].end
+                    }
+                ></FromToNumberInput>
+            </Fieldset>
+        {/each}
+    </Details>
 </section>
