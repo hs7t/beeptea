49cf7eb8f5aee6e9b0d5cd35cbcbb7538a04ee5b
Alí Sinisterra <haidarconh@gmail.com>
2025-10-20 14:10:17 -0500

    add Editing component to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Controls from "./lib/sections/Controls.svelte"
+    import Editing from "./lib/sections/Editing.svelte"
   import Encoding from "./lib/sections/Encoding.svelte";
   import Player from "./lib/sections/Player.svelte"
 </script>
@@ -8,6 +9,7 @@
     <Encoding />
     <Controls />
     <Player />
+    <Editing />
 </main>
 
 <style>
