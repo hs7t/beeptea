4dd107ea723dcd3a7f9f9ebcf1c818c6df02cf86
Alí Sinisterra <haidarconh@gmail.com>
2025-10-20 16:30:01 -0500

    create TextVisualizer component

---
+++ src/lib/components/TextVisualizer.svelte
@@ -0,0 +1,24 @@
+<script>
+    let { text } = $props()
+</script>
+
+<div class="text-visualizer">
+    <p>{text}</p>
+</div>
+
+<style>
+    .text-visualizer {
+        overflow-y: scroll;
+
+        display: flex;
+        align-items: center;
+    }
+
+    .text-visualizer p {
+        text-align: center;
+    }
+
+    .text-visualizer::-webkit-scrollbar {
+        display: none;
+    }
+</style>
