157ba8c51a8e4280bf87b2a935153a1b1fcf57a5
Alí Sinisterra <haidarconh@gmail.com>
2025-10-20 16:30:18 -0500

    replace Encoding textarea with input

--- src/lib/sections/Encoding.svelte
+++ src/lib/sections/Encoding.svelte
@@ -4,8 +4,14 @@
 </script>
 
 <section class="area encoding-area">
-    <textarea bind:value={appState.userInput.currentString}></textarea>
+    <input type="text" bind:value={appState.userInput.currentString} id="stringInput">
     <div>
         <p>{encodeString(appState.userInput.currentString)}</p>
     </div>
 </section>
+
+<style>
+    #stringInput {
+        width: 100%;
+    }
+</style>
