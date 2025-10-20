c4337c2ab1f3b576ec7bee50fb16444332a2e328
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-10-20 17:54:29 -0500

    style TextVisualizer

--- src/lib/components/TextVisualizer.svelte
+++ src/lib/components/TextVisualizer.svelte
@@ -8,17 +8,32 @@
 
 <style>
     .text-visualizer {
-        overflow-y: scroll;
-
+        overflow-x: scroll;
         display: flex;
         align-items: center;
+        border-radius: var(--b-radius-small);
+        border: var(--b-border-secondary);
+
+        padding: 0.8rem;
+        background-color: var(--b-color-overlay-background);
     }
 
     .text-visualizer p {
         text-align: center;
+        white-space: nowrap;
     }
 
     .text-visualizer::-webkit-scrollbar {
-        display: none;
+        width: 0.2rem;
+    }
+
+    .text-visualizer::-webkit-scrollbar-track {
+        background: #dcdcdc;
+        border-radius: 10pt;
+    }
+    
+    .text-visualizer::-webkit-scrollbar-thumb {
+        background: #b3b3b3;
+        border-radius: 10pt;
     }
 </style>
